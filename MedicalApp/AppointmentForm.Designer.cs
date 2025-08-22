d8c9dec48bead69839e5be7c84fc6f366e8637f4
nanagyanfosu <nanagyanfosu@gmail.com>
2025-08-22 19:41:36 +0000

    Appointment booking form done

---
+++ MedicalApp/AppointmentForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+namespace MedicalApp
+{
+    partial class AppointmentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDoctors = new System.Windows.Forms.ComboBox();
+            this.cmbPatients = new System.Windows.Forms.ComboBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbDoctors
+            // 
+            this.cmbDoctors.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDoctors.FormattingEnabled = true;
+            this.cmbDoctors.Location = new System.Drawing.Point(182, 79);
+            this.cmbDoctors.Name = "cmbDoctors";
+            this.cmbDoctors.Size = new System.Drawing.Size(242, 24);
+            this.cmbDoctors.TabIndex = 0;
+            this.cmbDoctors.SelectedIndexChanged += new System.EventHandler(this.cmbDoctors_SelectedIndexChanged);
+            // 
+            // cmbPatients
+            // 
+            this.cmbPatients.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPatients.FormattingEnabled = true;
+            this.cmbPatients.Location = new System.Drawing.Point(182, 126);
+            this.cmbPatients.Name = "cmbPatients";
+            this.cmbPatients.Size = new System.Drawing.Size(242, 24);
+            this.cmbPatients.TabIndex = 1;
+            this.cmbPatients.SelectedIndexChanged += new System.EventHandler(this.cmbPatients_SelectedIndexChanged);
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(182, 193);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(158, 22);
+            this.dtpDate.TabIndex = 2;
+            this.dtpDate.ValueChanged += new System.EventHandler(this.dtpDate_ValueChanged);
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(182, 259);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(242, 76);
+            this.txtNotes.TabIndex = 3;
+            this.txtNotes.Tag = "";
+            this.txtNotes.TextChanged += new System.EventHandler(this.txtNotes_TextChanged);
+            // 
+            // btnBook
+            // 
+            this.btnBook.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnBook.Location = new System.Drawing.Point(73, 389);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(164, 34);
+            this.btnBook.TabIndex = 4;
+            this.btnBook.Text = "Book Appointment";
+            this.btnBook.UseVisualStyleBackColor = false;
+            this.btnBook.Click += new System.EventHandler(this.btnBook_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Doctors:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Patients: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Date: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Notes: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(73, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(563, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Fill this form to book an appointment";
+            // 
+            // AppointmentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.cmbPatients);
+            this.Controls.Add(this.cmbDoctors);
+            this.Name = "AppointmentForm";
+            this.Text = "AppointmentForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void txtNotes_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dtpDate_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cmbPatients_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ;
+        }
+
+        private void cmbDoctors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDoctors;
+        private System.Windows.Forms.ComboBox cmbPatients;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox txtNotes;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ MedicalApp/AppointmentForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace MedicalApp
+{
+    public partial class AppointmentForm : Form
+    {
+        public AppointmentForm()
+        {
+            InitializeComponent();
+            LoadDoctors();
+            LoadPatients();
+        }
+
+        private void LoadDoctors()
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "SELECT DoctorID, FullName FROM Doctors WHERE Availability = 1";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                try
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(reader);
+
+                    cmbDoctors.DataSource = dt;
+                    cmbDoctors.DisplayMember = "FullName";
+                    cmbDoctors.ValueMember = "DoctorID";
+
+                    if (cmbDoctors.Items.Count > 0)
+                        cmbDoctors.SelectedIndex = 0; // force show first doctor
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading doctors: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        private void LoadPatients()
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "SELECT PatientID, FullName FROM Patients";
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                try
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(reader);
+
+                    cmbPatients.DataSource = dt;
+                    cmbPatients.DisplayMember = "FullName";
+                    cmbPatients.ValueMember = "PatientID";
+
+                    if (cmbPatients.Items.Count > 0)
+                        cmbPatients.SelectedIndex = 0; // force show first patient
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading patients: " + ex.Message);
+                }
+            }
+        }
+
+
+
+        private void btnBook_Click(object sender, EventArgs e)
+        {
+            if (cmbDoctors.SelectedValue == null || cmbPatients.SelectedValue == null)
+            {
+                MessageBox.Show("Please select both a doctor and a patient.");
+                return;
+            }
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+
+                string query = "INSERT INTO Appointments (DoctorID, PatientID, AppointmentDate, Notes) " +
+                               "VALUES (@DoctorID, @PatientID, @Date, @Notes)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@DoctorID", cmbDoctors.SelectedValue);
+                cmd.Parameters.AddWithValue("@PatientID", cmbPatients.SelectedValue);
+                cmd.Parameters.AddWithValue("@Date", dtpDate.Value);
+                cmd.Parameters.AddWithValue("@Notes", txtNotes.Text);
+
+               
+                int rows = cmd.ExecuteNonQuery();
+
+                if (rows > 0)
+                    MessageBox.Show("Appointment booked successfully!");
+                else
+                    MessageBox.Show("Failed to book appointment.");
+            }
+        }
+    }
+}
